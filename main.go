64191c94e1d898fcd6bd2c8cdaba6c1bd1a70adf
NEIHT1612 <cuongthienthong@gmail.com>
2025-10-24 14:31:25 +0700

    test transfer data by channel + config run build by launch.json

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	// "fmt"
+	// "time"
 	"example.com/m/v2/config"
 	"example.com/m/v2/models"
 	"example.com/m/v2/route"
@@ -16,4 +18,36 @@ func main() {
 	server := gin.Default()
 	route.RegisterRoutes(server)
 	server.Run(":8080")
+
+	// inputChan := make(chan int)
+	// processChan := make(chan int)
+	// outputChan := make(chan int)
+
+	// go func() {
+	// 	for i := 1; i <= 5; i++ {
+	// 		fmt.Println("Send to inputChan: ", i)
+	// 		inputChan <- i
+	// 		time.Sleep(300 * time.Millisecond)
+	// 	}
+	// 	close(inputChan)
+	// }()
+
+	// go func() {
+	// 	for num := range inputChan {
+	// 		fmt.Println("Processing: ", num)
+	// 		processChan <- num * 2
+	// 	}
+	// 	close(processChan)
+	// }()
+
+	// go func() {
+	// 	for num := range processChan {
+	// 		outputChan <- num + 1
+	// 	}
+	// 	close(outputChan)
+	// }()
+
+	// for result := range outputChan {
+	// 	fmt.Println("Final Result: ", result)
+	// }
 }
